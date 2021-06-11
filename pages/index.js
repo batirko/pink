7cc67868ce35a3fa709e6de54d0858c5961050f6
batirko <batirko@gmail.com>
2021-06-11 10:37:18 +0300

    css fix

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
         <title>Маркетинговое Агенство Розовый Слон</title>
         <meta name="description" content="Маркетинговое Агенство Розовый Слон" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width; initial-scale = 1.0; maximum-scale=1.0; user-scalable=no" />
       </Head>
 
       <main className={styles.main}>
