36eb20afe43d9a22f7429643aab240852cce2d43
batirko <batirko@gmail.com>
2021-06-11 12:50:39 +0300

    images

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -8,7 +9,6 @@ export default function Home() {
         <title>Маркетинговое Агенство Розовый Слон</title>
         <meta name="description" content="Маркетинговое Агенство Розовый Слон" />
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width; initial-scale = 1.0; maximum-scale=1.0; user-scalable=no" />
       </Head>
 
       <main className={styles.main}>
@@ -19,7 +19,7 @@ export default function Home() {
         <p className={styles.description}>
           Самые быстрые и качественные маркетинговые работы
         </p>
-
+        <Image src="/image.webp" height={300} width={300} />
         <div className={styles.grid}>
           <a className={styles.card}>
             <h2>Контент</h2>
@@ -47,12 +47,18 @@ export default function Home() {
             </p>
           </a>
         </div>
+        <div className={styles.end}>
+          <Image src="/image2.webp" height={1001 * 0.4} width={1083 * 0.4} objectFit="contain" />
+          <p>
+            Украина, г.Киев, ул. Автозаводская 54\19, офис 64 <br></br>
+        т. +380992918777 <br></br>
+        Маркетинговое Агенство Розовый Слон <br></br>
+          </p>
+        </div>
       </main>
 
       <footer className={styles.footer}>
-        Украина, г.Киев, ул. Автозаводская 54\19, офис 64 <br></br>
-        т. +380992918777 <br></br>
-        Маркетинговое Агенство Розовый Слон <br></br>
+        © 2021 Маркетинговое Агенство Розовый Слон, All Rights Reserved
       </footer>
     </div>
   )
